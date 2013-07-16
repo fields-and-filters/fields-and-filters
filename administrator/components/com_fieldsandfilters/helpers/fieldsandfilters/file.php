3be1f22b23b0fbb8f8f52ac1f4435c13f3a257bc
kextensions <kes@kextensions.com>
2013-07-16 23:53:28 +0200

    add file helper

---
+++ administrator/components/com_fieldsandfilters/helpers/fieldsandfilters/file.php
@@ -0,0 +1,96 @@
+<?php
+/**
+ * @version     1.0.0
+ * @package     com_fieldsandfilters
+ * @copyright   Copyright (C) 2012 KES - Kulka Tomasz . All rights reserved.
+ * @license     GNU General Public License version 3 or later; see License.txt
+ * @author      KES - Kulka Tomasz <kes@kextensions.com> - http://www.kextensions.com
+ */
+
+// No direct access
+defined( '_JEXEC' ) or die;
+
+jimport( 'joomla.filesystem.file' );
+
+/**
+ * Fieldsandfilters File.
+ * since       1.1.0
+ */
+class FieldsandfiltersFileHelper
+{
+        protected static $_mime_types = array(
+                                'txt' => 'text/plain',
+                                'htm' => 'text/html',
+                                'html' => 'text/html',
+                                'php' => 'text/html',
+                                'css' => 'text/css',
+                                'js' => 'application/javascript',
+                                'json' => 'application/json',
+                                'xml' => 'application/xml',
+                                'swf' => 'application/x-shockwave-flash',
+                                'flv' => 'video/x-flv',
+                                
+                                // images
+                                'png' => 'image/png',
+                                'jpe' => 'image/jpeg',
+                                'jpeg' => 'image/jpeg',
+                                'jpg' => 'image/jpeg',
+                                'gif' => 'image/gif',
+                                'bmp' => 'image/bmp',
+                                'ico' => 'image/vnd.microsoft.icon',
+                                'tiff' => 'image/tiff',
+                                'tif' => 'image/tiff',
+                                'svg' => 'image/svg+xml',
+                                'svgz' => 'image/svg+xml',
+                                
+                                // archives
+                                'zip' => 'application/zip',
+                                'rar' => 'application/x-rar-compressed',
+                                'exe' => 'application/x-msdownload',
+                                'msi' => 'application/x-msdownload',
+                                'cab' => 'application/vnd.ms-cab-compressed',
+                                
+                                // audio/video
+                                'mp3' => 'audio/mpeg',
+                                'qt' => 'video/quicktime',
+                                'mov' => 'video/quicktime',
+                                
+                                // adobe
+                                'pdf' => 'application/pdf',
+                                'psd' => 'image/vnd.adobe.photoshop',
+                                'ai' => 'application/postscript',
+                                'eps' => 'application/postscript',
+                                'ps' => 'application/postscript',
+                                
+                                // ms office
+                                'doc' => 'application/msword',
+                                'rtf' => 'application/rtf',
+                                'xls' => 'application/vnd.ms-excel',
+                                'ppt' => 'application/vnd.ms-powerpoint',
+                                
+                                // open office
+                                'odt' => 'application/vnd.oasis.opendocument.text',
+                                'ods' => 'application/vnd.oasis.opendocument.spreadsheet'
+        );
+        
+        public static function getMimeType( $file )
+	{
+		$ext = JFile::getExt( $file );
+		if( array_key_exists( $ext, self::$_mime_types ) )
+		{
+			return self::$_mime_types[$ext];
+		}
+		elseif( function_exists( 'finfo_open' ) )
+		{
+			$finfo 		= finfo_open( FILEINFO_MIME_TYPE );
+			$mimetype 	= finfo_file( $finfo, $file );
+			finfo_close( $finfo );
+			
+			return $mimetype;
+		}
+		else
+		{
+			return 'application/octet-stream';
+		}
+	}
+}
