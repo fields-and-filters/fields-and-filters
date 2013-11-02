e14f1353f3c8b66adf4d6c09901e6723e1fdb0b0
kextensions <kes@kextensions.com>
2013-11-02 21:54:31 +0100

    Add new simple syntax
    Simple syntax types: Simple syntax, Simple syntax + context, Simple syntax + params, Simple syntax + context + params, Old syntax
    
    Depracted: preparationConetent(), getFieldsByItemIDWithTemplate()

--- components/com_fieldsandfilters/helpers/fieldsandfilters/fieldssite.php
+++ components/com_fieldsandfilters/helpers/fieldsandfilters/fieldssite.php
@@ -17,10 +17,40 @@ JLoader::import( 'fieldsandfilters.factory', JPATH_ADMINISTRATOR . '/components/
 * @since       1.0.0
 */
 class FieldsandfiltersFieldsSiteHelper
-{	
+{
 	/**
-        * @since       1.1.0
-        */
+	 * Simple syntax - #{field_id,item_id-option}
+	 * @since       1.2.0
+	 **/
+	const SYNTAX_SIMPLE = 1;
+	
+	/**
+	 * Simple syntax + context - #{field_id,item_id-option,context}
+	 * @since       1.2.0
+	 **/
+	const SYNTAX_CONTEXT = 2;
+	
+	/**
+	 * Simple syntax + params - #{field_id,item_id-option,{params}}
+	 * @since       1.2.0
+	 **/
+	const SYNTAX_PARAMS = 3;
+	
+	/**
+	 * Simple syntax + context + params - #{field_id,item_id-option,context,{params}}
+	 * @since       1.2.0
+	 **/
+	const SYNTAX_CONTEXT_PARAMS = 4;
+	
+	/**
+	 * Old syntax - #{field_id,item_id,option}
+	 * @since       1.2.0
+	 **/
+	const SYNTAX_OLD = -1;
+	
+	/**
+	 * @since       1.1.0
+	 **/
 	public static function getFieldsByItemID( $option = null, $itemID = null, $fieldsID = null, $getAllextensions = true )
 	{
 		$app = JFactory::getApplication();
@@ -86,8 +116,280 @@ class FieldsandfiltersFieldsSiteHelper
 	}
 	
 	/**
-        * @since       1.1.0
-        */
+	 * @since       1.2.0
+	 **/
+	public static function getFieldsLayouts( JObject $object, $params = false, $ordering = 'ordering' )
+	{
+		$templateFields = new JObject;
+		
+		if( !isset( $object->fields ) || !isset( $object->element ) )
+		{
+			return $templateFields;
+		}
+		
+		$fields = $object->fields->getProperties();
+		
+		if( empty( $fields ) )
+		{
+			return $templateFields;
+		}
+		
+		$fields = new JObject( JArrayHelper::pivot( $fields, 'field_type' ) );
+		JPluginHelper::importPlugin( 'fieldsandfiltersTypes' );
+		
+		FieldsandfiltersFactory::getDispatcher()->trigger( 'getFieldsandfiltersFieldsHTML', array( $templateFields, $fields, $object->element, $params, $ordering ) );
+		
+		return $templateFields;
+	}
+	
+	/**
+	 * @since       1.2.0
+	 **/
+	public static function getFieldsLayoutsByItemID( $option = null, $itemID = null, $fieldsID = null, $getAllextensions = true, $params = false, $ordering = 'ordering' )
+	{
+		$object 	= self::getFieldsByItemID( $option, $itemID, $fieldsID, $getAllextensions );
+		return self::getFieldsLayouts( $object, $params, $ordering );
+	}
+	
+	/**
+	 * @since       1.2.0
+	 **/
+	public static function preparationContent( &$text, $context = '', $option = null, $itemID = null, $excluded = array(), $syntax = null, $syntaxType = null )
+	{
+		$syntax 	= $syntax ? $syntax : FieldsandfiltersFactory::getExtensions()->getPluginParams( 'system', 'fieldsandfilters' )->get( 'syntax', '#{%s}' );
+		$syntaxType 	= $syntaxType ? $syntaxType : FieldsandfiltersFactory::getExtensions()->getPluginParams( 'system', 'fieldsandfilters' )->get( 'syntax_type', self::SYNTAX_SIMPLE );
+		// [TODO] change interpolation to syntax
+		
+		if( strpos( $syntax, '%s' ) !== false )
+		{
+			$prefix = explode( '%s', $syntax );
+			
+			// simple performance check to determine whether bot should process further
+			if( !( $prefix = $prefix[0] ) || strpos( $text, $prefix ) === false )
+			{
+				return true;
+			}
+			
+			/* @deprecated  1.2.0 */
+			if( $syntaxType == self::SYNTAX_OLD )
+			{
+				$regexp = '(?P<field_id>\d+)(?:,(?P<item_id>\d+)|)(?:,(?P<option>[\w.-]+)|)';
+			}
+			/* @end deprecated  1.2.0 */
+			else
+			{
+				$regexp = '(?P<field_id>\d+)(?:,(?P<item_id>\d+|)(?:-(?P<option>[\w.-]+)|)|)';
+			
+				switch( $syntaxType )
+				{
+					case self::SYNTAX_CONTEXT:
+						$regexp .= '(?:,(?P<context>[\w.-]+)|)';
+					break;
+					case self::SYNTAX_PARAMS:
+						$regex .= '(?:,(?P<params>{.*?})|)';
+					break;
+					case self::SYNTAX_CONTEXT_PARAMS:
+						$regex .= '(?:,(?P<context>[^{.*?}][\w.-]+)|)(?:,(?P<params>{.*?})|)';
+					break;
+					case self::SYNTAX_SIMPLE:
+					default:
+						$syntaxType = self::SYNTAX_SIMPLE;
+					break;
+				}
+			}
+			
+			$regex = '/' . sprintf( $syntax, $regexp ) . '/i';
+			
+			// Find all instances of plugin and put in $matches for loadposition
+			// $matches[0] is full pattern match
+			preg_match_all( $regex, $text, $matches, PREG_SET_ORDER );
+			
+			if( !$matches )
+			{
+				return true;
+			}
+			
+			$jinput			= JFactory::getApplication()->input;
+			$itemID			= ( $itemID = (int) $itemID ) ? $itemID : $jinput->get( 'id', 0, 'int' );
+			$option			= $option ? $option : $jinput->get( 'option' );
+			$extensionsOptions 	= FieldsandfiltersFactory::getPluginExtensions()->getExtenionsOptions();
+			$combinations 		= array();
+			$getAllextensions 	= true;
+			$isExcluded		= !empty( $excluded ) && is_array( $excluded );
+			$excludes		= array();
+			$isParams		= $syntaxType == self::SYNTAX_PARAMS || $syntaxType == self::SYNTAX_CONTEXT_PARAMS;
+			
+			foreach( $matches as $match )
+			{
+				/* field id */
+				if( !( $fieldID = (int) $match['field_id'] ) )
+				{
+					$excludes[] = $match[0];
+					continue;
+				}
+				
+				/* context */
+				if( !empty( $match['context'] ) && $match['context'] != $context )
+				{
+					$excludes[] = $match[0];
+					continue;
+				}
+				
+				/* component - option + item id */
+				$_itemID        = ( isset( $match['item_id'] ) && ( $val = (int) $match['item_id'] ) ) ? $val : $itemID;
+				$_option         = !empty( $match['option'] ) ? $match['option'] : $option;
+				
+				if( !property_exists( $extensionsOptions, $_option ) )
+				{
+					$excludes[] = $match[0];
+					continue;
+				}
+				
+				$key = $_option . '-' . $_itemID;
+				if( !array_key_exists( $key, $combinations ) )
+				{
+					$combinations[$key] = array(
+						'item_id' 	=> $_itemID,
+						'option' 	=> $_option,
+						'fields_id'	=> array()
+					);
+					
+					if( $isParams )
+					{
+						$combinations[$key]['elements'] = array();
+					}
+					else
+					{
+						$combinations[$key]['matches'] = array();
+					}
+				}
+				
+				/* params */
+				if( $isParams )
+				{
+					$keyElement = $params = null;
+					if( !empty( $match['params'] ) )
+					{
+						$params 	= new JRegistry( $match['params'] );
+						$keyElement 	= $params->toString();
+						
+						if( $keyElement != '{}' )
+						{
+							$keyElement = md5( $keyElement );
+						}
+						else
+						{
+							$keyElement = $params = null;
+						}
+					}
+					
+					if( !array_key_exists( $keyElement, $combinations[$key]['elements'] ) )
+					{
+						$combinations[$key]['elements'][$keyElement] = array(
+							'matches'	=> array(),
+							'params'	=> $params
+						);
+					}
+					
+					$combinations[$key]['elements'][$keyElement]['matches'][$fieldID][] = $match[0];
+				}
+				else
+				{
+					$combinations[$key]['matches'][$fieldID] = $match[0];
+				}
+				
+								
+				if( !in_array( $fieldID, $combinations[$key]['fields_id'] ) )
+				{
+					$combinations[$key]['fields_id'][] = $fieldID;
+				}
+			}
+			
+			if( !empty( $combinations ) )
+			{
+				while( $combination = array_shift( $combinations ) )
+				{
+					$object = self::getFieldsByItemID( $combination['option'], $combination['item_id'], $combination['fields_id'], $getAllextensions );
+					
+					if( $isParams )
+					{
+						$isFields	= $object->fields->getProperties(true);
+						$isFields 	= !empty( $isFields );
+						
+						while( $element = array_shift( $combination['elements'] ) )
+						{
+							if( !$isFields )
+							{
+								$excludes = array_merge( $excludes, FieldsandfiltersFactory::getArray()->flatten( $element['matches'] ) );
+								continue;
+							}
+							
+							$_object 	= clone $object;
+							$_fieldsID 	= array_keys( $element['matches'] );
+							
+							foreach( $_fieldsID AS $_fieldID )
+							{
+								if( !isset( $_object->fields->$_fieldID ) )
+								{
+									unset( $_object->fields->$_fieldID );
+									
+									$excludes = array_merge( $excludes, $element['matches'][$_fieldID] );
+									unset( $element['matches'][$_fieldID] );
+								}
+							}
+							
+							$fieldsLayouts = self::getFieldsLayouts( $object, false, 'field_id' );
+							
+							foreach( $combination['matches'] AS $fieldID => &$match )
+							{
+								$text = str_replace( $match, addcslashes( $fieldsLayouts->get( $fieldID, '' ), '\\$' ), $text );
+							}
+						}
+					}
+					else
+					{
+						$fieldsLayouts = self::getFieldsLayouts( $_object, $element['params'], 'field_id' );
+							
+						foreach( $element['matches'] AS $fieldID => &$match )
+						{
+							$text = str_replace( $match, addcslashes( $fieldsLayouts->get( $fieldID, '' ), '\\$' ), $text );
+						}
+					}
+				}
+			}
+			
+			if( !empty( $excludes ) )
+			{
+				$text = str_replace( array_unique( $excludes ), addcslashes( '', '\\$' ), $text );
+			}
+		}
+		
+		return true;
+	}
+	
+	/**
+	 * @since       1.0.0
+	 **/
+	protected static function _returnEmpty()
+	{
+		return new JObject( array( 'element' => new JObject(), 'fields' => new JObject() ) );
+	}
+	
+	/**
+	 * @since       1.1.0
+	 * @deprecated  1.2.0
+	 * @use		FieldsandfiltersFieldsSite::preparationContent()
+	 **/
+	public static function preparationConetent( &$text, $option = null, $itemID = null, $syntax = null, $excluded = array() )
+	{
+		self::preparationContent( $text, '', $option, $itemID, $excluded, $syntax, self::SYNTAX_OLD );
+	}
+	
+	/**
+         * @since       	1.1.0
+         * @deprecated  	1.2.0
+         * @use		FieldsandfiltersFieldsSite::getFieldsLayoutsByItemID()
+         **/
 	public static function getFieldsByItemIDWithTemplate( $option = null, $itemID = null, $fieldsID = null, $getAllextensions = true, $params = false, $ordering = 'ordering' )
 	{
 		$object 	= self::getFieldsByItemID( $option, $itemID, $fieldsID, $getAllextensions );
@@ -106,108 +408,4 @@ class FieldsandfiltersFieldsSiteHelper
 		
 		return $templateFields;
 	}
-	
-	public static function preparationConetent( &$text, $option = null, $itemID = null, $interpolation = null, $excluded = array() )
-	{
-		$interpolation = $interpolation ? $interpolation : FieldsandfiltersFactory::getExtensions()->getPluginParams( 'system', 'fieldsandfilters' )->get( 'interpolation', '#{%s}' );
-		
-		// test new methods, what method is fasters
-		
-		if( strpos( $interpolation, '%s' ) !== false )
-		{
-			$regex = '/' . sprintf( $interpolation, '(.*?)' ) . '/i';
-			$prefix = explode( '%s', $interpolation );
-			
-			// simple performance check to determine whether bot should process further
-			if( !( $prefix = $prefix[0] ) || strpos( $text, $prefix ) === false )
-			{
-				return true;
-			}
-			
-			// Find all instances of plugin and put in $matches for loadposition
-			// $matches[0] is full pattern match, $matches[1] is the position
-			preg_match_all( $regex, $text, $matches, PREG_SET_ORDER );
-			
-			if( !$matches )
-			{
-				return true;
-			}
-			
-			$jinput			= JFactory::getApplication()->input;
-			$itemID			= ( $itemID = (int) $itemID ) ? $itemID : $jinput->get( 'id', 0, 'int' );
-			$option			= $option ? $option : $jinput->get( 'option' );
-			$extensionsOptions 	= FieldsandfiltersFactory::getPluginExtensions()->getExtenionsOptions();
-			$combinations 		= array();
-			$getAllextensions 	= true;
-			$isExcluded		= !empty( $excluded ) && is_array( $excluded );
-			$excludes		= array();
-			
-			foreach( $matches as $match )
-			{
-				$matcheslist 	= explode( ',', $match[1] );
-				
-				$fieldID 	= (int) $matcheslist[0];
-				if( $isExcluded && in_array( $fieldID, $excluded ) )
-				{
-					if( !in_array( $match[0], $excludes ) )
-					{
-						$excludes[] = $match[0];
-					}
-					continue;
-				}
-				
-				$_option = ( array_key_exists( 2, $matcheslist ) && ( $_option = trim( $matcheslist[2] ) ) ) ? $_option : $option;
-				if( !property_exists( $extensionsOptions, $_option ) )
-				{
-					if( !in_array( $match[0], $excludes ) )
-					{
-						$excludes[] = $match[0];
-					}
-					continue;
-				}
-				
-				$_itemID 	= ( array_key_exists( 1, $matcheslist ) && ( $_itemID = (int) $matcheslist[1] ) ) ? $_itemID : $itemID;
-				$key 		= $_option . '-' . $_itemID;
-				
-				if( !array_key_exists( $key, $combinations ) )
-				{
-					$combinations[$key] = array(
-									'item_id' 	=> $_itemID,
-									'option' 	=> $_option,
-									'matches'	=> array()
-							);
-				}
-				
-				$combinations[$key]['fields_id'][] = $fieldID;
-				$combinations[$key]['matches'][$fieldID] = $match[0];
-			}
-			
-			if( !empty( $combinations ) )
-			{
-				while( $combination = array_shift( $combinations ) )
-				{
-					$fields = self::getFieldsByItemIDWithTemplate( $combination['option'], $combination['item_id'], $combination['fields_id'], $getAllextensions, false, 'field_id' );
-					
-					foreach( $combination['matches'] AS $fieldID => &$match )
-					{
-						$text = str_replace( $match, addcslashes( $fields->get( $fieldID, '' ), '\\$' ), $text );
-					}
-				}
-			}
-			
-			if( !empty( $excludes ) )
-			{
-				$text = str_replace( $excludes, addcslashes( '', '\\$' ), $text );
-			}
-		}
-	}
-	
-	/**
-        * @since       1.0.0
-        */
-	protected static function _returnEmpty()
-	{
-		return new JObject( array( 'element' => new JObject(), 'fields' => new JObject() ) );
-	}
-	
 }
