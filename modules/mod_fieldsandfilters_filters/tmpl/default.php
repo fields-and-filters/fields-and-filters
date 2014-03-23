7424266dad32f63c9f5be48bc4033c97124d40fa
kextensions <kes@kextensions.com>
2014-03-23 22:56:11 +0100

    + missing language translation

--- modules/mod_fieldsandfilters_filters/tmpl/default.php
+++ modules/mod_fieldsandfilters_filters/tmpl/default.php
@@ -14,20 +14,20 @@ $isReset = $params->get( 'show_reset', 0 );
 ?>
 	
 <div id="faf-mod-<?php echo $module->id; ?>" class="faf-mod">
-        <form action="#" method="get" name="faf-form-<?php echo $module->id; ?>"
-                id="faf-form-<?php echo $module->id; ?>" class="faf-filters-form faf-form faf-filters-loading form-inline" autocomplete="off">
+        <form action="#" method="get" name="faf-form-<?php echo $module->id; ?>" id="faf-form-<?php echo $module->id; ?>" class="faf-filters-form faf-form faf-filters-loading form-inline" autocomplete="off">
                 
                 <?php echo implode( "\n", (array) $filters->get('layouts') ); ?>
-                
+
 		<?php if( !$isReset ) : ?>
-			<input type="reset" id="faf-form-empty-<?php echo $module->id; ?>" name="faf-form-empty-<?php echo $module->id; ?>" class="btn btn-link faf-form-empty" value="<?php echo JText::_( 'PLG_FAF_ES_CT_ERROR_NOT_MATCH_TO_FILTERS_RESET' );?>" />
+			<input type="reset" id="faf-form-empty-<?php echo $module->id; ?>" name="faf-form-empty-<?php echo $module->id; ?>" class="btn btn-link faf-form-empty" value="<?php echo JText::_( 'MOD_FILEDSANDFILTERS_FILTERS_ERROR_NOT_MATCH_TO_FILTERS_RESET' );?>" />
 		<?php endif; ?>
 		
-                <?php if( $params->get( 'show_submit', 1 ) ) : ?>
-                <input type="submit" id="faf-form-submit-<?php echo $module->id; ?>" name="faf-form-submit-<?php echo $module->id; ?>" class="btn btn-primary faf-form-submit" value="<?php echo JText::_( 'MOD_FILEDSANDFILTERS_FORM_SUBMIT' );?>" />
-                <?php endif; ?>
+		<?php if( $params->get( 'show_submit', 1 ) ) : ?>
+                <input type="submit" id="faf-form-submit-<?php echo $module->id; ?>" name="faf-form-submit-<?php echo $module->id; ?>" class="btn btn-primary faf-form-submit" value="<?php echo JText::_( 'MOD_FILEDSANDFILTERS_FILTERS_FORM_SUBMIT' );?>" />
+		<?php endif; ?>
+
 		<?php if( $isReset ) : ?>
-                <input type="reset" id="faf-form-reset-<?php echo $module->id; ?>" name="faf-form-reset-<?php echo $module->id; ?>" class="btn btn-info faf-form-reset" value="<?php echo JText::_( 'MOD_FILEDSANDFILTERS_FORM_RESET' );?>" />
-                <?php endif; ?>
+                <input type="reset" id="faf-form-reset-<?php echo $module->id; ?>" name="faf-form-reset-<?php echo $module->id; ?>" class="btn btn-info faf-form-reset" value="<?php echo JText::_( 'MOD_FILEDSANDFILTERS_FILTERS_FORM_RESET' );?>" />
+		<?php endif; ?>
         </form>
 </div>
