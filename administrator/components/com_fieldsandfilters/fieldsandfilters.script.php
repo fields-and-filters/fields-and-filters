efc92e6da76ed5fa64891b3f37ee0fadb57e897a
kextensions <kes@kextensions.com>
2014-03-28 18:15:28 +0100

    - methods addExtensions and unistall in fieldsandfilters.script.php

--- administrator/components/com_fieldsandfilters/fieldsandfilters.script.php
+++ administrator/components/com_fieldsandfilters/fieldsandfilters.script.php
@@ -20,16 +20,6 @@ class com_fieldsandfiltersInstallerScript
 {
 	protected $helper;
 
-	protected $extensions_queue = array(
-			'plg_system_fieldsandfilters',
-			'plg_fieldsandfiltersextensions_content',
-			'plg_fieldsandfilterstypes_checkboxlist',
-			'plg_fieldsandfilterstypes_image',
-			'plg_fieldsandfilterstypes_input',
-			'plg_fieldsandfilterstypes_textarea',
-			'mod_fieldsandfilters_filters'
-	);
-
 	protected static $remove_folders = array(
 		'/administrator/components/com_fieldsandfilters/helpers',
 		'/components/com_fieldsandfilters/helpers'
@@ -83,70 +73,6 @@ class com_fieldsandfiltersInstallerScript
 		if ($type == 'install' || ($type == 'update'))
 		{
 			$helper->checkContentTypes('com_fieldsandfilters.allextensions');
-			return $this->addExtensions();
-		}
-		
-		return true;
-        }
-	
-	/**
-         * Method to uninstall the extension
-         * $adapter is the class calling this method
-         *
-         * @return void
-         */
-        public function uninstall($adapter) 
-        {
-		if (empty($this->extensions_queue) || !is_array($this->extensions_queue))
-		{
-			return;
-		}
-		
-		$app 	= JFactory::getApplication();
-		$table 	= JTable::getInstance('extension');
-		
-		$eid	= $app->input->get('cid', array(), 'array');
-		JArrayHelper::toInteger($eid, array());
-		
-		while ($extension = array_shift($this->extensions_queue))
-		{
-			$installer      = new JInstaller;
-			
-			$table->reset();
-			switch (substr($extension, 0, 4))
-			{
-				case 'mod_' :
-					$table->load(array('element' => $extension));
-				break;
-				case 'plg_' :
-					list($type, $folder, $element) = explode('_', $extension);
-					$table->load(array(
-							'folder' 	=> $folder,
-							'element' 	=> $element,
-					));
-				break;
-				default :
-					continue;
-				break;
-			}
-			
-			if (in_array($table->extension_id, $eid) || !$table->extension_id || !$table->type)
-			{
-				continue;
-			}
-			
-			if ($installer->uninstall($table->type, $table->extension_id))
-			{
-				// Extension uninstalled sucessfully
-				$msg = JText::sprintf('COM_FIELDSANDFILTERS_SUCCESS_UNINSTALL_EXTENSION', $extension);
-				$app->enqueueMessage($msg);
-			}
-			else
-			{
-				// Extension uninstalled error
-				$msg = JText::sprintf('COM_FIELDSANDFILTERS_ERROR_NOT_UNINSTALL_EXTENSION', $extension);
-				$app->enqueueMessage($msg, 'error');
-			}
 		}
 		
 		return true;
@@ -256,100 +182,6 @@ class com_fieldsandfiltersInstallerScript
 	protected function getHelper()
 	{
 		return $this->helper;
-	}
-	
-	protected function addExtensions()
-	{
-		$helper = $this->getHelper();
-		
-		// Get an installer instance
-		$app	        = JFactory::getApplication();
-		$parent		= $helper->getAdapter()->getParent();
-		$manifest	= $parent->getManifest();
-		
-		if ($manifest->administration->files)
-		{
-			$element = $manifest->administration->files;
-		}
-		elseif ($manifest->files)
-		{
-			$element = $manifest->files;
-		}
-		else
-		{
-			$element = null;
-		}
-		
-		$folder = $element ? ('/' . (string) $element->attributes()->folder) : '/administrator';
-		
-		$sourcePath     = $parent->getPath('source');
-		$adminPath	= $sourcePath . $folder;
-		$extensionsPath = $adminPath . '/extensions';
-		
-		// Load language
-		$helper->getAdapter()->loadLanguage($adminPath);
-		
-		if (!is_dir($extensionsPath) || empty($this->extensions_queue) || !is_array($this->extensions_queue))
-		{
-			$app->enqueueMessage(JText::_('COM_FIELDSANDFILTERS_ERROR_NOT_EXISTS_EXTENSIONS'), 'error');
-			return false;
-		}
-		
-		$table = JTable::getInstance('extension');
-		while ($extension = array_shift($this->extensions_queue))
-		{
-			$installer      = new JInstaller;
-			$extensionDir 	= $extensionsPath . '/' . $extension;
-			
-			if (!($installer->install($extensionDir)))
-			{
-				// There was an error installing the package
-				$msg = JText::sprintf('COM_FIELDSANDFILTERS_ERROR_NOT_INSTALL_EXTENSION', $extension);
-				$app->enqueueMessage($msg, 'error');
-			}
-			else
-			{
-				$type = (string) $installer->manifest->attributes()->type;
-				if ($type == 'plugin')
-				{
-					$table->reset();
-					unset($element);
-					
-					// Set the installation path
-					if (count($installer->manifest->files->children()))
-					{
-						foreach ($installer->manifest->files->children() as $file)
-						{
-							if ((string) $file->attributes()->$type)
-							{
-								$element = (string) $file->attributes()->$type;
-								break;
-							}
-						}
-					}
-					
-					$folder = (string) $installer->manifest->attributes()->group;
-					
-					if (!empty($folder) && !empty($element) && $table->load(array('folder' => $folder, 'element' => $element, 'enabled' => 0)))
-					{
-						$table->enabled = 1;
-						
-						if (!$table->store())
-						{
-							// false
-						}
-					}	
-				}
-				
-				// Extension installed/updated sucessfully
-				$route = strtolower($installer->getAdapter($type)->get('route'));
-				$text = ($route == 'update') ? 'COM_FIELDSANDFILTERS_SUCCESS_UPGRADE_EXTENSION' : 'COM_FIELDSANDFILTERS_SUCCESS_INSTALL_EXTENSION';
-				$msg = JText::sprintf($text, $extension);
-				$app->enqueueMessage($msg);
-			}
-		}
-		
-		return true;
 	}
 	
 	protected static function loadClass($class, $adapter)
